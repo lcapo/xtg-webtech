60b063787c193e5b74c485970e6099694fa5d39f
oscar <oscar@xmltravelgate.com>
2016-02-18 09:44:46 +0100

    shs

--- run-docker.sh
+++ run-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm --label=jekyll --volume=$(pwd):/srv/jekyll -it -p 127.0.0.1:4000:4000 jekyll/jekyll
+docker run --rm --label=jekyll --volume=$(pwd):/srv/jekyll -it -p 127.0.0.1:4000:4000 xtg/jekyll
